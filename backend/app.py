e883e161399bb2ed9818c3e7ffe9a6050c2ac511
Jflores427 <Flores.Josue427@gmail.com>
2025-02-08 21:29:49 -0500

    Implemented Backend for Authentication involving login,register, logout

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,16 @@ from flask import Flask, session, redirect, url_for, jsonify, request
 from routes.auth import auth_bp
 from routes.discussion import discussion_bp
 import psycopg2
+from flask_jwt_extended import JWTManager
 from config import Config
+from routes.register import register_bp
+from routes.login import login_bp
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
+app.config["JWT_SECRET_KEY"] = Config.SECRET_KEY
+jwt = JWTManager(app)
 
 # Function to connect to the Supabase Postgres database
 def get_db_connection():
@@ -16,6 +21,8 @@ def get_db_connection():
 # Register the authentication blueprint
 app.register_blueprint(auth_bp)
 app.register_blueprint(discussion_bp, url_prefix='/discussion')
+app.register_blueprint(register_bp, url_prefix="/auth")
+app.register_blueprint(login_bp, url_prefix="/auth")
 
 @app.route("/")
 def index():
--- backend/config.py
+++ backend/config.py
@@ -7,3 +7,5 @@ load_dotenv()
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES = 3600  # 1 hour
