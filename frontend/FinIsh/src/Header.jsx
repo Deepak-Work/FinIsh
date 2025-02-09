893c38e4d21a56edc72f05e7b7fc0dfb024b28b3
DeepakSingh260 <alexmha260@gmail.com>
2025-02-09 08:00:36 +0530

    Frontend

--- frontend/FinIsh/src/Header.jsx
+++ frontend/FinIsh/src/Header.jsx
@@ -9,10 +9,10 @@ function Header() {
       </div>
       <nav className="nav">
         <a href="/">Profile</a>
-        <a href="/enrollment">Enrollment</a>
-        <a href="/discussion">Discussion</a>
-        <a href="/explore">Explore</a>
-        <a href="/aboutus">About Us</a>
+        <a href="/#enrollment">Enrollment</a>
+        <a href="/#discussion">Discussion</a>
+        <a href="/#explore">Explore</a>
+        <a href="/#aboutus">About Us</a>
       </nav>
     </header>
   );
